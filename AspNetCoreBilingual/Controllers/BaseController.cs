3f06afc717134d9efc2375512d23d5658b8fd1a7
GT <26967905+bigguy96@users.noreply.github.com>
2021-04-23 18:31:09 -0400

    work in progress on alternate language.

--- AspNetCoreBilingual/Controllers/BaseController.cs
+++ AspNetCoreBilingual/Controllers/BaseController.cs
@@ -1,8 +1,14 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace AspNetCoreBilingual.Controllers
 {
@@ -11,24 +17,42 @@ namespace AspNetCoreBilingual.Controllers
         // GET
         public override void OnActionExecuting(ActionExecutingContext context)
         {
-            //var culture = context.RouteData.Values["culture"].ToString();
-            //var alternateCulture = culture.Equals("en") ? "fr" : "en";
-            //var path = string.Empty;
+            var requestCulture = Request.HttpContext.Features.Get<IRequestCultureFeature>();
+            var currentCulture = requestCulture.RequestCulture.Culture.TwoLetterISOLanguageName;
+            var alternateCulture = currentCulture.Equals("en") ? "fr" : "en";
+            var baseUri = Request.GetDisplayUrl();
+            var uri = new Uri(Request.GetDisplayUrl());
+            var query = QueryHelpers.ParseQuery(uri.Query);
+            var items = query.SelectMany(x => x.Value, (col, value) => new KeyValuePair<string, string>(col.Key, value)).ToList();
+            var fullUri = baseUri;
+
+            if (Request.Query.ContainsKey("lang"))
+            {
+                items.RemoveAll(x => x.Key == "lang"); // Remove all values for key
+                var qb = new QueryBuilder(items) { { "lang", alternateCulture } };
+
+                fullUri += qb.ToQueryString();
+            }
+            else
+            {
+                var qb = new QueryBuilder { { "lang", alternateCulture } };
+                fullUri += qb.ToQueryString();
+            }
 
             //if (context.ActionDescriptor is ControllerActionDescriptor controllerActionDescriptor)
             //{
-            //    var actionAttributes = controllerActionDescriptor.MethodInfo.GetCustomAttributes(inherit: true)
-            //        .Cast<HttpGetAttribute>()
-            //        .SingleOrDefault(x => !x.Template.Contains(culture));
+            //    //var actionAttributes = controllerActionDescriptor.MethodInfo.GetCustomAttributes(inherit: true)
+            //    //    .Cast<HttpGetAttribute>()
+            //    //    .SingleOrDefault(x => !x.Template.Contains(culture));
 
-            //    var regex = new Regex(@"/{(.*?)}");
-            //    if (actionAttributes != null)
-            //    {
-            //        path = regex.Replace(actionAttributes.Template, "");
-            //    }
+            //    //var regex = new Regex(@"/{(.*?)}");
+            //    //if (actionAttributes != null)
+            //    //{
+            //    //    path = regex.Replace(actionAttributes.Template, "");
+            //    //}
             //}
 
-            //ViewData["Toggle"] = $"/{alternateCulture}{path}";
+            ViewData["Toggle"] = fullUri;
 
             base.OnActionExecuting(context);
         }
@@ -39,3 +63,6 @@ namespace AspNetCoreBilingual.Controllers
         }
     }
 }
+
+
+//https://benjii.me/2017/04/parse-modify-query-strings-asp-net-core/
