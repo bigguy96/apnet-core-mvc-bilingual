84371640c53c015f9d62003b4f9f4ac8b93477a0
GT <26967905+bigguy96@users.noreply.github.com>
2021-04-24 09:19:49 -0400

    Removed comments.

--- AspNetCoreBilingual/Startup.cs
+++ AspNetCoreBilingual/Startup.cs
@@ -1,10 +1,6 @@
-using System.Collections.Generic;
 using System.Globalization;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
-using Microsoft.AspNetCore.Localization.Routing;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,16 +17,8 @@ namespace AspNetCoreBilingual
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.Configure<CookiePolicyOptions>(options =>
-            //{
-            //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-            //    options.CheckConsentNeeded = context => true;
-            //    options.MinimumSameSitePolicy = SameSiteMode.None;
-            //});
-
             services
                 .AddLocalization(o => o.ResourcesPath = "Resources")
                 .AddRouting()
