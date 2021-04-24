acd8b83687d73df5d3ddb0044d9bf998bb041fcd
GT <26967905+bigguy96@users.noreply.github.com>
2021-04-24 09:17:32 -0400

    Added a post method to test the localization.

--- AspNetCoreBilingual/Controllers/ServicesController.cs
+++ AspNetCoreBilingual/Controllers/ServicesController.cs
@@ -1,16 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreBilingual.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreBilingual.Controllers
 {
     [Route("services")]
     public class ServicesController : BaseController
     {
-        // GET
         [HttpGet("dashboard-tableaudebord")]
         public IActionResult Index()
         {
             SetPageTitle("Services");
-            return View();
+            return View(new ServiceViewModel());
+        }
+
+        [HttpPost("dashboard-tableaudebord")]
+        public IActionResult Index(ServiceViewModel serviceViewModel)
+        {
+            SetPageTitle("Services");
+            return View(serviceViewModel);
         }
     }
 }
---
+++ AspNetCoreBilingual/Models/ServiceViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AspNetCoreBilingual.Models
+{
+    public class ServiceViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
